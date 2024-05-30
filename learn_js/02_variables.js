94874aee096b81c8bfd55b22315c42f54273f63d
Nikhil Patel <115778633+np2861996@users.noreply.github.com>
2024-05-30 14:14:11 +0000

    variables and datatypes

---
+++ learn_js/02_variables.js
@@ -0,0 +1,8 @@
+const userName = 'nikhil_patel';
+let accountId = 0245;
+var ContactNumber = 9023039563;
+emailId = "nikhil@gmail.com";
+
+console.log(userName)
+
+console.table([userName, accountId, ContactNumber, emailId])
---
+++ learn_js/03_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+foo = true; // foo is now a boolean
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
